4ac2a873e9f6ff3c0467485a8a16ca7fdfb4f3b1
Stephen Bowers <sb@thatstephenbowers.com>
2021-04-17 19:17:43 -0700

    Add Gatsby to repo

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FourOhFourPage() {
+  return <p>Sorry, that page doesn't exist!</p>;
+}
---
+++ gatsby/src/pages/potions.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PotionsPage() {
+  return <div>Potions Page</div>
+}
