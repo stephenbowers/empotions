19e420e52ce65d01af4c41fb17c2b48ee17fd7ac
Stephen Bowers <sb@thatstephenbowers.com>
2021-05-03 18:37:20 -0700

    Add specialties and social media to Person schema

--- sanity/schemas/person.js
+++ sanity/schemas/person.js
@@ -31,5 +31,36 @@ export default {
                 hotspot: true,
             },
         },
+        {
+            name: 'specialties',
+            title: 'Specialties',
+            type: 'array',
+            description: 'Areas of expertise',
+            of: [{ type: 'string'}],
+        },
+        {
+            name: 'facebook',
+            title: 'Facebook',
+            type: 'string',
+            description: 'Facebook username',
+        },
+        {
+            name: 'twitter',
+            title: 'Twitter',
+            type: 'string',
+            description: 'Twitter username',
+        },
+        {
+            name: 'pinterest',
+            title: 'Pinterest',
+            type: 'string',
+            description: 'Pinterest username',
+        },
+        {
+            name: 'tiktok',
+            title: 'TikTok',
+            type: 'string',
+            description: 'TikTok username',
+        },
     ],
 };
